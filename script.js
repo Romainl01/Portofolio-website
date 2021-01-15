6f4b251627df774e1823f1d2cfc1fd77f8d8656f
Romain  Lagrange <romainlagrange33@gmail.com>
2021-01-15 11:46:43 +0100

    responsive and a lot of improvements

--- script.js
+++ script.js
@@ -11,7 +11,17 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
-
+// Dark Mode
 function toggleDarkMode(){
   document.body.classList.toggle("dark-mode");
 }
+
+// responsive header
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+    mainNav.classList.toggle('main-nav');
+});
